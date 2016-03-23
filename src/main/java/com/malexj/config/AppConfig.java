a4fb231190ed72b4a0a1882032a1ab3b6ee66257
malexj <alexmaximovj@gmail.com>
2016-03-23 22:48:06 +0200

    init project

---
+++ src/main/java/com/malexj/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.malexj.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.malexj.")
+public class AppConfig {
+
+}
---
+++ src/main/webapp/resources/js/module/module.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// DRY роутинг в AngularJS:  https://habrahabr.ru/post/190958/
+var myApp = angular.module('myApp', []);
